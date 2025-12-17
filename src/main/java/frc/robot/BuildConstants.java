2f7f6e03b640c0e42d83d5b51a7cead50074e717
da-supreme-n0ob <harrychess3@gmail.com>
2025-12-16 21:03:25 -0600

    idk what this is

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "harry2025test";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "37d7e873295b8ec24faa10ad2c4540e507161b2a";
-  public static final String GIT_DATE = "2025-12-03 15:52:32 CST";
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "695b59f974f83d1d5cf5c2cc9384e99f8808c413";
+  public static final String GIT_DATE = "2025-12-16 20:58:12 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-12-13 13:06:58 CST";
-  public static final long BUILD_UNIX_TIME = 1765652818047L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-12-16 20:59:43 CST";
+  public static final long BUILD_UNIX_TIME = 1765940383454L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
