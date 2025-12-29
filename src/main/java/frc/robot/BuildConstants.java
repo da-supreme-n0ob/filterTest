a7c79d16f297c5d30bba92cce5d038d4a3281422
da-supreme-n0ob <harrychess3@gmail.com>
2025-12-29 08:20:05 -0600

    removed latency factor in stddev filter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "harry2025test";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "695b59f974f83d1d5cf5c2cc9384e99f8808c413";
-  public static final String GIT_DATE = "2025-12-16 20:58:12 CST";
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "2f7f6e03b640c0e42d83d5b51a7cead50074e717";
+  public static final String GIT_DATE = "2025-12-16 21:03:25 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-12-16 20:59:43 CST";
-  public static final long BUILD_UNIX_TIME = 1765940383454L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-12-21 17:48:19 CST";
+  public static final long BUILD_UNIX_TIME = 1766360899884L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- src/main/java/team5427/lib/filters/soft/otherFilters/standardDeviation.java
+++ src/main/java/team5427/lib/filters/soft/otherFilters/standardDeviation.java
@@ -11,7 +11,6 @@ public class standardDeviation{
     private double ambiguity;
     private double area;
     private double distance;
-    private double latency;
     private double timeElapsed;
 
     //balancing based on research, can alter if necessary
@@ -20,7 +19,6 @@ public class standardDeviation{
     private final double nominalArea = 0.02;
     private final double epsilon = 1e-6;//helps prevent division by zero when area is too small
     private final double distanceWeight = 0.15;
-    private final double latencyWeight = 1.5;
     private final double timeElapsedWeight = 2;
 
     public standardDeviation(String cameraName){
@@ -28,7 +26,6 @@ public class standardDeviation{
         this.ambiguity = camera.getPoseAmbiguity();
         this.area = camera.getTargetArea();
         this.distance = getDistance(camera);
-        this.latency = camera.getResultLatency();
         this.timeElapsed = Timer.getFPGATimestamp()-camera.getResultTimestamp();
         calculate();
     }
@@ -51,16 +48,15 @@ public class standardDeviation{
         double distancePart = distanceWeight*distance*distance;
         double ambiguityPart = ambiguityWeight*ambiguity;
         double areaPart = areaWeight*(nominalArea/(area+epsilon));
-        double latencyPart = latencyWeight*latency;
         double timeElapsedPart = timeElapsedWeight*timeElapsed;
 
         //formula
-        double factor = 1+distancePart+ambiguityPart+areaPart+latencyPart+timeElapsedPart;
+        double factor = 1+distancePart+ambiguityPart+areaPart+timeElapsedPart;
         newStandardDeviation=baseStandardDeviation*factor;
 
         //rejects if certain things are too extreme
         if(distance>7||ambiguity>0.35||area<0.003||newStandardDeviation>3){
-            newStandardDeviation=-1;
+            newStandardDeviation=100;
         }
     }
     public double getStandardDeviation(){
